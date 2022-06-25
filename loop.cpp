ce5ab4a9619097c6571f60d1c152033abfe8f472
Jacob Adamczyk <42879357+JacobHA@users.noreply.github.com>
2022-06-25 10:47:59 -0400

    simple loop

---
+++ loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int j = 0;
+    for(int i = 0; i < 10; i++)
+    {
+        j = j + i;
+        cout << j << endl;
+    }
+
+    
+    return 0;
+
+}
